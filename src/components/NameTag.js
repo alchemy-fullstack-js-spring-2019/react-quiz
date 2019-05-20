4766f34f8ec8ed15774128d139bb57e657654665
Ryan Mehta <ryan.mehta@gmail.com>
2019-05-20 13:26:29 -0700

    answer

---
+++ src/components/NameTag.js
@@ -0,0 +1,38 @@
+import React, { PureComponent } from 'react';
+import TextSelector from './TextSelector';
+import Shape from './Shape';
+import ColorSelector from './ColorSelector';
+
+export default class NameTag extends PureComponent {
+  state = {
+    text: '',
+    color: '#000000',
+    backgroundColor: '#FF0000'
+  }
+
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { text, color, backgroundColor } = this.state;
+    return (
+      <>
+        <TextSelector
+          text={text}
+          handleChange={this.handleChange}
+        />
+        <ColorSelector
+          color={color}
+          backgroundColor={backgroundColor}
+          handleChange={this.handleChange}
+        />
+        <Shape
+          text={text}
+          color={color}
+          backgroundColor={backgroundColor}
+        />
+      </>
+    );
+  }
+}
---
+++ src/components/Shape.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Shape({ text, color, backgroundColor }) {
+  const styles = {
+    width: '100px',
+    height: '100px',
+    color,
+    backgroundColor
+  };
+  return (
+    <div style={styles}>
+      <p>{text}</p>
+    </div>
+  );
+}
+
+Shape.propTypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired
+};
+
+export default Shape;
