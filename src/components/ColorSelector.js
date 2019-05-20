87c0b569286766cd40dedffcbb7e9d8fbafa41d1
Emily Creamer <emily.creamer@icloud.com>
2019-05-20 09:57:15 -0700

    ColorSelector, no test

---
+++ src/components/ColorSelector.js
@@ -0,0 +1,21 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+
+function ColorSelector({ color, backgroundColor, onChange }) {
+  return (
+    <>
+      <h1>Text Color</h1>
+      <input name="color" color={color} type="color" onChange={onChange} />
+      <h1>Background Color</h1>
+      <input name="backgroundColor" backgroundColor={backgroundColor} type="color" onChange={onChange} />
+    </>
+  );
+}
+
+ColorSelector.propTypes = {
+  color: PropTypes.string.isRequired,
+  onChange: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired
+};
+
+export default ColorSelector;
