2c3329623cbd9c8d5ba6770735a6d3ee6be48cff
Cheri Portman <cheriportman@hotmail.com>
2019-05-20 10:30:28 -0700

    that's that

--- src/components/TextSelector.js
+++ src/components/TextSelector.js
@@ -4,24 +4,40 @@ import PropTypes from 'prop-types';
 //presentational
 //text input
 
-export default class TextSelector extends PureComponent {
 
+export default class TextSelector extends PureComponent {
+  
   state = {
     text: 'starter text'
   };
-
+  
   handleChange = (event) => {
     this.setState({value: event.target.value});
   }
-
+  
   render() {
-
+    
     return (
       <input type="text" value={text} onChange={this.handleChange}></input>
-    );
-  }
-
-};
-
-
-
+      );
+    }
+    
+  };
+  
+  // function TextSelector({ text }) {
+  //   const { text } = text;
+  //   return (
+  //     <section>
+  //       <input type="text"></input>
+  //     </section>
+  //   );
+  // }
+  
+  // TextSelector.propTypes = {
+  //   text: PropTypes.string.isRequired
+  // };
+  
+  // export default TextSelector;
+  
+  
+  
