b805da7a66298bee7c93e468e202d569806d885b
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-05-20 10:03:47 -0700

    shape pres component, set up

---
+++ src/components/Shape.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Shape({ text, color, backgroundColor }) {
+  return(
+    <section>
+      <p style={{ color:{color} }}>{text}</p>
+      <div style={{ width: '100px', height: '100px', backgroundColor={backgroundColor}}}></div>
+    </section>
+  )
+}
+
+Shape.proptypes = {
+  text: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  backgroundColor: PropTypes.string.isRequired
+}
+export default Shape;
